2bd96dc182db194d7058c5178f2b500a0cd03bdc
abun880007 <abun880007@msn.com>
2019-02-26 16:44:39 +0000

    update TUB

--- tub_starlte.mk
+++ tub_starlte.mk
@@ -1,23 +1,15 @@
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 $(call inherit-product, device/samsung/starlte/full_starlte.mk)
-$(call inherit-product, device/samsung/universal9810-common/universal9810-common.mk)
 
-# Inherit some common tub stuff.
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/tub/config/common.mk)
 $(call inherit-product, vendor/tub/config/telephony.mk)
 
-	PRODUCT_DEVICE := starlte
-	PRODUCT_NAME := tub_starlte
-
-    TARGET_GAPPS_ARCH :=arm64
-		
-# Device fingerprint
 BUILD_FINGERPRINT := samsung/starltexx/starlte:8.0.0/R16NW/G960FXXU1ARCC:user/release-keys
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
+ PRODUCT_BUILD_PROP_OVERRIDES += \
         PRIVATE_BUILD_DESC="starltexx-user 8.0.0 R16NW G960FXXU1ARCC release-keys"
+        
+PRODUCT_NAME := tub_starlte
